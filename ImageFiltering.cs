65c3731433ba42800c2c1dd71b942baffc8877bd
Ivo Gonçalves <kaly.imsg@gmail.com>
2019-03-23 00:20:24 +0100

    Initial Commit

---
+++ ImageFiltering.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Kalimage
+{
+    class ImageFiltering
+    {
+        private Bitmap _rawImage;
+
+        public ImageFiltering(Bitmap image)
+        {
+            _rawImage = image;
+        }
+
+        public Bitmap MedianFilter(int size)
+        {
+            // original code at:
+            // http://www.gutgames.com/post/Noise-Reduction-of-an-Image-in-C-using-Median-Filters.aspx
+            Bitmap TempBitmap = (Bitmap)_rawImage;
+            Bitmap NewBitmap = new Bitmap(TempBitmap.Width, TempBitmap.Height);
+            Graphics NewGraphics = Graphics.FromImage(NewBitmap);
+            NewGraphics.DrawImage(TempBitmap, new Rectangle(0, 0, TempBitmap.Width, TempBitmap.Height), new Rectangle(0, 0, TempBitmap.Width, TempBitmap.Height), System.Drawing.GraphicsUnit.Pixel);
+            NewGraphics.Dispose();
+            Random TempRandom = new Random();
+            for (int x = 0; x < NewBitmap.Width; ++x)
+            {
+                for (int y = 0; y < NewBitmap.Height; ++y)
+                {
+                    List<int> RValues = new List<int>();
+                    List<int> GValues = new List<int>();
+                    List<int> BValues = new List<int>();
+                    int ApetureMin = -(size / 2);
+                    int ApetureMax = (size / 2);
+                    for (int x2 = ApetureMin; x2 < ApetureMax; ++x2)
+                    {
+                        int TempX = x + x2;
+                        if (TempX >= 0 && TempX < NewBitmap.Width)
+                        {
+                            for (int y2 = ApetureMin; y2 < ApetureMax; ++y2)
+                            {
+                                int TempY = y + y2;
+                                if (TempY >= 0 && TempY < NewBitmap.Height)
+                                {
+                                    Color TempColor = TempBitmap.GetPixel(TempX, TempY);
+                                    RValues.Add(TempColor.R);
+                                    GValues.Add(TempColor.G);
+                                    BValues.Add(TempColor.B);
+                                }
+                            }
+                        }
+                    }
+                    RValues.Sort();
+                    GValues.Sort();
+                    BValues.Sort();
+                    Color MedianPixel = Color.FromArgb(RValues[RValues.Count / 2], GValues[GValues.Count / 2], BValues[BValues.Count / 2]);
+                    NewBitmap.SetPixel(x, y, MedianPixel);
+                }
+            }
+            return NewBitmap;
+        }
+    }
+}
